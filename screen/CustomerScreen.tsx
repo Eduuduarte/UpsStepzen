4bb9cc814a2db85c073dd077cf48dcad7f17a581
Eduardo Duarte <eduardoievolucao@gmail.com>
2023-01-13 21:37:08 -0300

    created customer screen

---
+++ screen/CustomerScreen.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View, SafeAreaView } from 'react-native';
+import { useTailwind } from 'tailwind-rn/dist';
+
+const CustomerScreen = () => {
+    const tw = useTailwind();
+
+    return (
+      <SafeAreaView>
+        <Text style={tw("text-blue-500")}> textInComponent </Text>
+      </SafeAreaView>
+    )
+
+}
+
+export default CustomerScreen
