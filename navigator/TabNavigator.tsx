bd59eb1dd4ff308121e6bd77584c1486e988d795
Eduardo Duarte <eduardoievolucao@gmail.com>
2023-01-16 19:57:08 -0300

    built the customers screen

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -6,45 +6,45 @@ import { useNavigation } from '@react-navigation/native';
 import { Icon } from '@rneui/themed';
 
 export type TabStackParamList = {
-    Customers: undefined;
-    Orders: undefined;
+  Customers: undefined;
+  Orders: undefined;
 }
 
 const Tab = createBottomTabNavigator<TabStackParamList>();
 
 const TabNavigator = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false,
-        });
-    }, [])
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [])
 
   return (
-    <Tab.Navigator screenOptions={({ route }) =>({
+    <Tab.Navigator screenOptions={({ route }) => ({
       tabBarActiveTintColor: "#59C1CC",
       tabBarInactiveTintColor: "gray",
       tabBarIcon: ({ focused, color, size }) => {
-        if(route.name === 'Customers') {
+        if (route.name === 'Customers') {
           return (
-            <Icon 
-              name= "users"
+            <Icon
+              name="users"
               type="entypo"
               color={focused ? "#59C1CC" : "gray"}
             />
           )
         } else if (route.name === 'Orders') {
           return (
-            <Icon 
-              name= "box"
+            <Icon
+              name="box"
               type="entypo"
               color={focused ? "#EB6A7C" : "gray"}
             />
           )
         }
       }
-      })}>
+    })}>
       <Tab.Screen name="Customers" component={CustomerScreen} />
       <Tab.Screen name="Orders" component={OrdersScreen} />
     </Tab.Navigator>
--- screen/CustomerScreen.tsx
+++ screen/CustomerScreen.tsx
@@ -1,15 +1,45 @@
-import React, { Component } from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Image, Input } from '@rneui/themed';
+import React, { useState, useLayoutEffect } from 'react';
+import { Text, SafeAreaView, ScrollView, ActivityIndicator } from 'react-native';
 import { useTailwind } from 'tailwind-rn/dist';
+import { RootStackParamList } from '../navigator/RootNavigatior';
+import { TabStackParamList } from '../navigator/TabNavigator';
+
+export type CustomerScreenNavigationProp =
+  CompositeNavigationProp<BottomTabNavigationProp<TabStackParamList, 'Customers'>, NativeStackNavigationProp<RootStackParamList>>;
+
+
 
 const CustomerScreen = () => {
-    const tw = useTailwind();
+  const tw = useTailwind();
+  const navigation = useNavigation<CustomerScreenNavigationProp>();
+  const [input, setInput] = useState<string>('');
 
-    return (
-      <SafeAreaView>
-        <Text style={tw("text-blue-500")}> textInComponent </Text>
-      </SafeAreaView>
-    )
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [])
+
+  return (
+    <ScrollView style={{ backgroundColor: '#59C1CC' }}>
+      <Image
+        source={{ uri: 'https://links.papareact.com/3jc' }}
+        containerStyle={tw("w-full h-64")}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+
+      <Input
+        placeholder='Search by Customer'
+        value={input}
+        onChangeText={text => setInput(text)}
+        containerStyle={tw("bg-white pt-5 pb-0 px-10")}
+      />
+    </ScrollView>
+  )
 
 }
 
