27627249508f026ad9e73a5e1e0226580e0a1632
Eduardo Duarte <eduardoievolucao@gmail.com>
2023-01-21 21:10:31 -0300

    changes apollo client

--- App.tsx
+++ App.tsx
@@ -3,9 +3,11 @@ import utilities from './tailwind.json';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigator/RootNavigatior';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import React from 'react';
 
 const client = new ApolloClient({
-  uri: 'https://dashboard.stepzen.com/explorer?endpoint=api%2Funrealistic-gecko',
+  uri: 'https://mandan.stepzen.net/api/unrealistic-gecko/__graphql',
+  headers: {'Authorization': 'apikey mandan::stepzen.io+1000::767541bf28b60120a5fa60c26bacbd77ead8a28303df3e44d788e5245649a279'},
   cache: new InMemoryCache(),
 });
 
